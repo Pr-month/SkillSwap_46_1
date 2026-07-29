1ad6d1fbd590ff123a10348b703b4d357035da70
Sergei74660 <sergei74660@gmail.com>
2026-07-29 20:04:09 +0300

    feat(auth): add JwtPayload interface

---
+++ src/auth/auth.types.ts
@@ -0,0 +1,9 @@
+export interface JwtPayload {
+  sub: string;                          // ID пользователя
+  email: string;                        // email
+  tokenType: 'access' | 'refresh';      // чтобы отличать access от refresh
+  iat?: number;
+  exp?: number;
+}
+
+export type JwtPayloadInput = Pick<JwtPayload, 'sub' | 'email'>;
