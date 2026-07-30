7f94402eeb454cb7ae08db84f9f62f89cc32dae3
Azat Ziganshin <azat_z@office.lan>
2026-07-30 10:05:07 +0300

    feat(src): add JWT module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './app.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -12,6 +13,13 @@ import { configuration } from './app.config';
       isGlobal: true,
       load: [configuration],
       envFilePath: '.env', 
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET, // временно берём напрямую из env
+      signOptions: { 
+        expiresIn: process.env.JWT_EXPIRES_IN || '1d' as any, // аналогично временное решение
+      },
     }),
     UsersModule, 
     AuthModule],
