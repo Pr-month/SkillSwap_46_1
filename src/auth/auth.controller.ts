4743db6ed9668779a52fe9c152399bd9f51005d4
ZigaQuasar <azat-physick@ya.ru>
2026-08-03 00:46:36 +0300

    feat(auth): create auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,24 @@
 import {
+  Body,
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
   HttpCode,
   HttpStatus,
+  Patch,
+  Post,
+  Request,
+  UseGuards
 } from '@nestjs/common';
 
-import { AuthService } from './auth.service';
-import { UpdatePasswordDto } from './dto/update-password.dto';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { RegisterDto, RegisterResponseDto } from './dto/register.dto';
 import { BusinessException } from '../common/errors/business.exception';
 import { exceptionCodes } from '../common/errors/error-codes';
+import { AuthService } from './auth.service';
+import { LoginDto, LoginResponseDto } from './dto/login.dto';
+import { RegisterDto, RegisterResponseDto } from './dto/register.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -52,11 +55,47 @@ export class AuthController {
   @ApiOperation({ summary: 'Вход в систему' })
   @ApiResponse({ status: 200, type: LoginResponseDto })
   async login(@Request() req, @Body() _: LoginDto) {
-    // @Body() _: LoginDto нужно только для Swagger-документации
     return this.authService.login(req.user);
   }
 
+   @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @ApiOperation({ summary: 'Получение профиля текущего пользователя' })
+  async getProfile(@Request() req) {
+    try {
+      return await this.authService.getProfile(req.user.id);
+    } catch (error) {
+      if (error instanceof BusinessException) {
+        throw error;
+      }
+      throw new BusinessException(
+        exceptionCodes.common.internal,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-
-  
+  @UseGuards(JwtAuthGuard)
+  @Patch('password')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Смена пароля' })
+  async updatePassword(
+    @Request() req,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    try {
+      return await this.authService.updatePassword(
+        req.user.id,
+        updatePasswordDto,
+      );
+    } catch (error) {
+      if (error instanceof BusinessException) {
+        throw error;
+      }
+      throw new BusinessException(
+        exceptionCodes.common.internal,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  } 
 }
