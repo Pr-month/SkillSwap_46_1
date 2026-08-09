99fb181fc1c7cbc96d83dcb0c1a6153e53c08200
Ilya <146587316+duckpicker@users.noreply.github.com>
2026-08-09 15:22:55 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StringValue } from 'ms';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
 import { SubcategoriesModule } from './categories/subcategories/subcategories.module';
 import { dbConfig } from './config/db.config';
 import { jwtConfigFactory } from './config/jwt.config';
@@ -49,4 +58,5 @@ import { UsersModule } from './users/users.module';
   ],
   controllers: [AppController],
   providers: [AppService],
-  
+})
+export class AppModule {}
